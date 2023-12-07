7c45f7792c05640d59c3bdbc27ca157c67d1e301
Paul Fornage <36117326+paulwrath1223@users.noreply.github.com>
2023-12-07 00:03:37 -0800

    Basically init

---
+++ run.bat
@@ -0,0 +1,20 @@
+@echo off
+cd DosBox
+set /p com_port= "Enter the COM port of the Radio (e.g. COM3): "
+echo %com_port%
+cd RSS
+echo -------------------
+echo available models:
+echo -------------------
+dir /B /D
+echo -------------------
+set /p radio_model= "Enter the radio model (e.g. gp300): "
+echo %radio_model%
+cd ..
+echo serial1=directserial realport:%com_port%> temp_config.conf
+echo serial2=directserial realport:%com_port%> temp_config.conf
+echo serial3=directserial realport:%com_port%> temp_config.conf
+echo serial4=directserial realport:%com_port%> temp_config.conf
+echo cycles=211>> temp_config.conf
+dosbox RSS\%radio_model%\%radio_model%.exe -exit -conf temp_config -noconsole
+
